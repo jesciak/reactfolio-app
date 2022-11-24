9c264226e65ec7d225fadf08396e26c119efa2e9
jesciak <jesciak@yahoo.com>
2022-11-23 22:09:25 -0500

    added Navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Navbar from '../components/Navbar'
 
 
 export default function Home(){
-    return <h1>Home</h1>
+    return (
+        <main className="text-tan-300 bg-brown-800 body-font">
+   
+    <h1>Home</h1>
+    </main>
+)
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 
-export default function Navbar(){
-    return 
-    // <h1>Navbar</h1>
-    
+export default function Navbar() {
+    const linkStyle = { border: '2px tan', padding: '5px' };
+    return (
+        <nav className="main-header-menu">
+            <section style={{
+                display: 'flex',
+                fontFamily: 'arial',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start'
+            }}
+            >
+                <div style={linkStyle}>
+                    <a href="#">Home</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">About</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Portfolio</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Contact</a>
+                </div>
+            </section>
+        </nav>
+    );
 }
+
