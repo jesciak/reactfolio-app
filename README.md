2788ee617c197d9476476323494bee3bec29109f
jesciak <jesciak@yahoo.com>
2022-11-23 21:19:21 -0500

    set up components

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Reactfolio
+
+ ![MIT License](https://img.shields.io/badge/license-MIT-brightgreen)
+## Description
 React application project portfolio webpage
+## Usage
+
+
+ ![screenshot](/assets/)
+  ![screenshot](/assets/)
+## License
+MIT License<br>
+http://choosealicense.com/licenses/mit/<br>
+  ## Contributors
+
+[@jesciak](https://github.com/jesciak/)
+
+## Links
--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Home from './components/Home'
+import About from './components/About'
+import Navbar from './components/Navbar'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main className="text-tan-300 bg-brown-800 body-font">
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Portfolio/>
+    <Contact/>
+    <Footer/>
 
-export default App;
+  </main>
+  );
+  }
+
